4dd02cbec4869179fb3b85b46f0c5d0da2aa7bf2
bhavika <bhavika.isha@gmail.com>
2020-04-29 13:17:39 +0530

    Completed feature 5: search feature with book not found

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BookListComponent } from './components/book-list/book-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule,Routes } from '@angular/router';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { BookCategoryComponent } from './components/book-category/book-category.component'; 
+import { BookCategoryComponent } from './components/book-category/book-category.component';
+import { SearchComponent } from './components/search/search.component'; 
 
 const routes: Routes = [
   {path: 'books', component: BookListComponent},
+  {path: 'search/:keyword', component: BookListComponent},
   {path: 'category/:id', component: BookListComponent},
   {path: '', redirectTo: '/books', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
@@ -20,7 +22,8 @@ const routes: Routes = [
     AppComponent,
     BookListComponent,
     PageNotFoundComponent,
-    BookCategoryComponent
+    BookCategoryComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -13,6 +13,7 @@ export class BookListComponent implements OnInit {
   
   books: Book[];
   currentCategoryId: number;
+  searchMode: boolean;
 
   constructor(private _bookService: BookService,
               private _activatedRoute: ActivatedRoute) { }
@@ -26,7 +27,16 @@ export class BookListComponent implements OnInit {
   }
 
   listBooks(){
+    this.searchMode = this._activatedRoute.snapshot.paramMap.has('keyword');
 
+    if(this.searchMode){
+      this.handleSearchBooks();
+    }else{
+      this.handleListBooks();
+    }
+  }
+
+  handleListBooks(){
     const hasCategoryId: boolean = this._activatedRoute.snapshot.paramMap.has('id');
 
     if(hasCategoryId){
@@ -38,6 +48,17 @@ export class BookListComponent implements OnInit {
     this._bookService.getBooks(this.currentCategoryId).subscribe(
         data => this.books = data
     )
+  }
+
+  handleSearchBooks(){
+    const keyword: string = this._activatedRoute.snapshot.paramMap.get('keyword');
+
+    this._bookService.searchBooks(keyword).subscribe(
+      data => {
+        this.books = data;
+      }
+    )
 
   }
+
 }
